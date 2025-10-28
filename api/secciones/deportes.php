3dfdfa6b8d63ee0c45449f1b007fb21c658acb20
ZapataHarukichi <zapatacarbonel@gmail.com>
2025-10-28 18:38:53 -0500

    Add files via upload

--- api/secciones/deportes.php
+++ api/secciones/deportes.php
@@ -7,6 +7,6 @@ $deportes = [
 "titulo" => "'Flaco' Granda y su categórico calificativo a Universitario tras lograr el tricampeonato: Como los grandes equipos europeos",
 "autor" => "Fabrizio Jáuregui",
 "resumen" => "El reconocido periodista consideró que Universitario de Deportes pasó por encima a todos sus rivales en la temporada. Asimismo, remarcó que lograron el título con su sello.",
-"imagen" => "/imagenes/universitario.avif",
+"imagen" => "/imagenes/universitario.webp",
 ];
 ?>
